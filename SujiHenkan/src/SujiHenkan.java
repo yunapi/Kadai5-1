27e485ff7c3f940ef6e4686610d5f679b1d04fa4
yunapi <g12936ny@s30450.tsuda.ac.jp>
2014-05-13 11:07:22 +0900

    1000までやったよ ドライバー中西 ナビゲーター桑原

--- SujiHenkan/src/SujiHenkan.java
+++ SujiHenkan/src/SujiHenkan.java
@@ -36,7 +36,7 @@ public class SujiHenkan {
 		else if(n>90 && n<100) str="ninety-" +i[n-90];
 		else if(n==100) str="one hundred";
 		else if(n>100 && n<=120) str="one hundred-"+i[n-100];
-		else if(n>120 && n<200)
+		else if(n>120 && n<1000)
 			str=translateEng1(n);
 		
 			
@@ -46,10 +46,44 @@ public class SujiHenkan {
 	
 	static String translateEng1(int n) {
 
-		String str1=translateEng(n-100);
 		String str2="";
-	    if(n>120 && n<200);
-	    str2="one hundred-"+str1;
+		
+		if(n>120 && n<200)
+	    str2="one hundred-"+translateEng(n-100);
+	    else if(n==200)
+	    str2="two hundred";
+	    else if(n>200 && n<300)
+	    	str2="two hundred-"+translateEng(n-200);
+	    else if(n==300)
+	    	str2="three hundred";
+	    else if(n>300 && n<400)
+	    	str2="three hundred-"+translateEng(n-300);
+	    else if(n==400)
+	    	str2="four hundred";
+	    else if(n>400 && n<500)
+	    	str2="four hundred-"+translateEng(n-400);
+	    else if(n==500)
+	    	str2="five hundred";
+	    else if(n>500 && n<600)
+	    	str2="five hundred-"+translateEng(n-500);
+	    else if(n==600)
+	    	str2="six hundred";
+	    else if(n>600 && n<700)
+	    	str2="six hundred-"+translateEng(n-600);
+	    else if(n==700)
+	    	str2 = "seven hundred";
+	    else if(n>700 && n<800)
+	    	str2="seven hundred-"+translateEng(n-700);
+	    else if(n==800)
+	    	str2="eight hundred";
+	    else if(n>800 && n<900)
+	    	str2="eight hundred-"+translateEng(n-800);
+	    else if(n==900)
+	    	str2="nine hundred";
+	    else if(n>900 && n<1000)
+	    	str2="ninehundred-"+translateEng(n-900);
+	    
+	    	
 	    return str2;
 	
 	}
